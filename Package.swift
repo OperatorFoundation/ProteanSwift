ffbacfe0725e05296fcd5a8ef61c5a9dfc7c317f
CryptoSax <joshua@operatorfoundation.org>
2021-11-10 15:21:21 -0600

    updated dependencies

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ProteanSwift",
-    platforms: [.macOS(.v11)],
+    platforms: [.macOS(.v10_15)],
     products: [
         .library(name: "ProteanSwift", targets: ["ProteanSwift"])],
     dependencies: [
-        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.0.5"),
+        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.1.2"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.4.0"),
     ],
     targets: [
